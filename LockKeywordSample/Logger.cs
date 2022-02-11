0231b7d99c3fc9ed4336148453ad17c6c858c3d9
1HostGhost1 <furkan_aslan2209@hotmail.com>
2022-02-11 11:56:51 +0300

    Add project files.

---
+++ LockKeywordSample/Logger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockKeywordSample
+{
+    public class Logger
+    {
+        private static Logger _logger;
+        private static object _lockObject = new object();
+
+        //Made this so it wouldn't get inheritance from another class except this
+        private Logger() { }
+
+
+        //Create instance without lock
+        public static Logger CreateWithoutLock()
+        {
+            if (_logger == null)
+            {
+                _logger = new Logger();
+                Info($"{nameof(Logger)} created");
+            }
+            Info($"Exist {nameof(Logger)} returned");
+            return _logger;
+        }
+
+        //Create instance with lock
+        public static Logger CreateWithLock()
+        {
+            lock (_lockObject)
+            {
+                if (_logger == null)
+                {
+                    _logger = new Logger();
+                    Info($"{nameof(Logger)} created");
+                }
+                Info($"Exist {nameof(Logger)} returned");
+                return _logger;
+            }
+        }
+
+        public static void Info(string message) => Console.WriteLine($"Info --> {message}");
+        //public void Error(string message) => Console.WriteLine($"Error --> {message}");
+    }
+}
---
+++ LockKeywordSample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LockKeywordSample
+{
+    class Program
+    {
+        delegate void AsnycMethodHandler();
+        static void Main(string[] args)
+        {
+            AsnycMethodHandler asnycMethod = () =>
+            {
+                ////Run this without locking
+                Logger withoutLock = Logger.CreateWithoutLock();
+
+                ////Run this with locking
+                //Logger withLock = Logger.CreateWithLock();
+            };
+
+            #region If you're using .Net Core run these
+            Task.Run(() => asnycMethod.Invoke());
+            Task.Run(() => asnycMethod.Invoke());
+            Task.Run(() => asnycMethod.Invoke());
+            Task.Run(() => asnycMethod.Invoke());
+            Task.Run(() => asnycMethod.Invoke());
+            #endregion
+
+            #region If you're using .Net run these
+            //asnycMethod.BeginInvoke(new AsyncCallback((x) => { }), new object());
+            //asnycMethod.BeginInvoke(new AsyncCallback((x) => { }), new object());
+            //asnycMethod.BeginInvoke(new AsyncCallback((x) => { }), new object());
+            //asnycMethod.BeginInvoke(new AsyncCallback((x) => { }), new object());
+            //asnycMethod.BeginInvoke(new AsyncCallback((x) => { }), new object());
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
